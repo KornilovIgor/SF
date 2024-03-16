ee636a36b465c1f8d264ca7a5694a64e2796310c
KornilovIgor <mail@kote.dev>
2024-03-16 14:32:48 +0300

    qSave

--- trie/src/main.cpp
+++ trie/src/main.cpp
@@ -1,7 +1,21 @@
+#include "Trie.h"
+
 #include <iostream>
 
 int main()
 {
+    TrieNode* root = getNewNode();
+    insert(root, "apple");
+    insert(root, "application");
+    insert(root, "apply");
+    insert(root, "book");
+    insert(root, "box");
+
+    char buffer[100];
+    string result;
+    findMinPrefixes(root, buffer, 0, result);
+
+    cout << "Minimal Prefixes: " << result << endl;
 
     return 0;
 }
