0931328696c1078824a6520c38a6353d267f5228
KornilovIgor <mail@kote.dev>
2024-03-26 00:36:40 +0300

    HTC is done 2

--- HTC/include/Hash.h
+++ HTC/include/Hash.h
@@ -1,5 +1,5 @@
 #pragma once
-
+/*
 #include <string.h>
 
 #define NAMELENGTH 10  // Зафикcируем длину имени
@@ -66,7 +66,75 @@ private:
         enPairStatus status;
     };
 
+    void resize();
+
     int hash_func(FruitName fr_name, int offset);
+
+    Pair* array;
+    int mem_size;
+    int count;
+};*/
+
+
+#include <string.h>
+
+#define NAMELENGTH 10  // Зафикируем длину имени
+
+typedef char FruitName[NAMELENGTH]; // тип имя фрукта
+
+class HashTable { // хэш таблица
+public:
+
+    HashTable();
+    ~HashTable();
+    void add(FruitName fr_name, int fr_count);
+
+    friend void test(HashTable& ht);
+
+    void dummy2(void** p);
+private:
+
+    enum enPairStatus {
+        free,
+        engaged,
+        deleted
+    };
+
+    struct Pair { // пара ключ-значение
+
+        Pair() :
+            fruit_name(""),
+            fruit_count(-1),
+            status(enPairStatus::free)
+        {}
+        Pair(FruitName fr_name, int fr_count) :
+            fruit_count(fr_count),
+            status(enPairStatus::engaged) {
+            strcpy(fruit_name, fr_name);
+        }
+        Pair& operator = (const Pair& other) {
+            fruit_count = other.fruit_count;
+            strcpy(fruit_name, other.fruit_name);
+            status = other.status;
+
+            return *this;
+        }
+
+        bool operator == (const Pair& other) {
+            return  status == other.status &&
+                (status != enPairStatus::engaged || (fruit_count == other.fruit_count && !strcmp(fruit_name, other.fruit_name)));
+        }
+
+        FruitName fruit_name;
+        int fruit_count;
+
+        enPairStatus status;
+    };
+
+    void resize();
+    int hash_func(FruitName fr_name, int offset);
+
+    void dummy(int a);
 
     Pair* array;
     int mem_size;
--- HTC/src/Hash.cpp
+++ HTC/src/Hash.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 #include <string.h>
-
+/*
 HashTable::HashTable() {
     count = 0;
     mem_size = 8;
@@ -13,6 +13,11 @@ HashTable::~HashTable() {
 }
 void HashTable::add(FruitName fr_name, int fr_count)
 {
+    if (count >= mem_size)
+    {
+        resize();
+    }
+
     int index = -1, i = 0;
     // берем пробы по всем i от 0 до размера массива
     for (; i < mem_size; i++)
@@ -113,4 +118,127 @@ int HashTable::find(FruitName fr_name) {
         }
     }
     return -1;
+}*/
+/*
+void HashTable::resize()
+{
+    int new_mem_size = mem_size * 2;
+
+    Pair* new_array = new Pair[new_mem_size];
+
+    for (int i = 0; i < mem_size; i++)
+    {
+        if (array[i].status == enPairStatus::engaged)
+        {
+            int index = -1, j = 0;
+            for (; j < new_mem_size; j++)
+            {
+                index = hash_func(array[i].fruit_name, j);
+                if (new_array[index].status == enPairStatus::free)
+                {
+                    break;
+                }
+            }
+
+            if (j >= new_mem_size)
+            {
+                delete[] new_array;
+                return;
+            }
+
+            new_array[index] = array[i];
+        }
+    }
+
+    delete[] array;
+
+    array = new_array;
+    mem_size = new_mem_size;
+}
+*/
+
+HashTable::HashTable() {
+    count = 0;
+    mem_size = 8;
+    array = new Pair[mem_size];
+}
+HashTable::~HashTable() {
+    delete[] array;
+}
+int HashTable::hash_func(FruitName fr_name, int offset) {
+    // вычисл€ем индекс
+    int sum = 0, i = 0;
+    for (; i < strlen(fr_name); i++) {
+        sum += fr_name[i];
+    }
+    // линейные пробы
+    return (sum % mem_size + offset) % mem_size;
+}
+
+void HashTable::resize() {
+    // Ќе забудьте добавить вызов resize() в нужное место
+    // внутри метода add, а затем добавить элемент в увеличенный массив
+    // ¬аш код
+}
+
+void HashTable::add(FruitName fr_name, int fr_count) {
+    /* ¬ этом методе нужно добавить вызов resize() */
+
+
+    int index = -1, i = 0;
+    // берем пробы по всем i от 0 до размера массива
+    for (; i < mem_size; i++) {
+        index = hash_func(fr_name, i);
+        if (array[index].status == enPairStatus::free) {
+            // найдена пуста€ €чейка, занимаем ее
+            break;
+        }
+    }
+
+    if (i >= mem_size) return; // все перебрали, нет места
+
+    // кладем в свободную €чейку пару
+    array[index] = Pair(fr_name, fr_count);
+    count++;
+}
+
+
+/*–ешение автора
+
+ void HashTable::resize() {
+
+    Pair* save = array;
+    int save_ms = mem_size;
+
+    mem_size *= 2;
+    array = new Pair[mem_size];
+    count = 0;
+
+    for(int i=0; i<save_ms; i++) {
+        Pair& old_pair = save[i];
+        if(old_pair.status == enPairStatus::engaged) {
+            add(old_pair.fruit_name, old_pair.fruit_count);
+        }
+    }
+
+    delete[] save;
+}
+void HashTable::add(FruitName fr_name, int fr_count) {
+    int index = -1, i = 0;
+    // берем пробы по всем i от 0 до размера массива
+    for(;i < mem_size; i++) {
+        index = hash_func(fr_name, i);
+        if(array[index].status == enPairStatus::free) {
+            // найдена пуста€ €чейка, занимаем ее
+            break;
+        }
+    }
+
+    if(i >= mem_size) {
+        resize();
+        add(fr_name, fr_count);
+    } else {
+        array[index] = Pair(fr_name, fr_count);
+        count++;
+    }
 }*/
