64a2a5ccc4a3e37dd646dddf650db0feace071ca
KornilovIgor <mail@kote.dev>
2024-04-16 13:32:56 +0300

    makefile is done

---
+++ make/MyLib.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#include <string>
+
+void text_out(const std::string& text); //print text to console
---
+++ make/main.cpp
@@ -0,0 +1,8 @@
+#include "MyLib.hpp"
+
+int main()
+{
+    text_out("Privet from make utility!");
+
+    return 0;
+}
